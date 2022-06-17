36427ef581f0b5bfff4d4ee005842fa1f2947dab
Shhh <shanwilkinson2@users.noreply.github.com>
2022-06-17 21:59:42 +0100

    cleaning top row blank

--- read and merge files.R
+++ read and merge files.R
@@ -36,46 +36,64 @@ library(janitor)
   names(files_list2) <- downloaded_files$file_date
   
 # compare variables in list elements
-  colname_mismatches <- compare_df_cols(files_list2,
-                  return = "mismatch") %>%
-    data.table::transpose()
-  names(colname_mismatches)<- colname_mismatches[1,]
-  colname_mismatches <- colname_mismatches[-1,] 
-  colname_mismatches <- colname_mismatches %>%
-    mutate(file_date = names(files_list2))
+  compare_mismatches <- function(objectname){
+      colname_mismatches <- compare_df_cols(objectname,
+                      return = "mismatch") %>%
+        data.table::transpose()
+      names(colname_mismatches)<- colname_mismatches[1,]
+      colname_mismatches <- colname_mismatches[-1,] 
+      colname_mismatches <- colname_mismatches %>%
+        mutate(file_date = names(objectname))
+      View(colname_mismatches)
+  }
 
   colname_mismatches %>% filter(!is.na(v1))
   
 # cleaning
   # january-2018 - 1st col unnamed, seems to be an id number
   # june-2021 - first row blank, probably empty columns at the end
+  # payment date is a mix of number (date squished into number)& text (date with -)
   
-  
-  files_list3 <- map(.x = files_list2,
+  # remove empty columns
+  files_list2 <- map(.x = files_list2,
                      .f = ~janitor::remove_empty(.x, "cols"))
 
-  
-  # identify if all col names are v1, v2, v3 etc
-  current_names <- names(files_list3[[1]])
-  current_names <- current_names[1:(length(current_names)-1)]
-  
-  test_names <- 1:(length(current_names))
-  test_names <- paste0("v", test_names)
-  
-  test_2 <- data.frame(current_names = current_names, 
-                       test_names = test_names) %>%
-    mutate(match = ifelse(current_names == test_names, TRUE, FALSE))
-  
-
-  if(sum(test_2$mismatch > 0)) {
-    message("blank names")
+ # check for blank names 
+  check_toprow_blank <- function(filename, i){
+    test_names <- 1:100
+    test_names <- paste0("v", test_names) 
+    
+    # identify if all col names are v1, v2, v3 etc
+    current_names <- names(filename[[i]])
+    current_names <- current_names[1:(length(current_names)-1)]
+    
+    test_2 <- data.frame(current_names = current_names, 
+                         test_names = test_names[1:length(current_names)]) %>%
+      mutate(match = ifelse(current_names == test_names, TRUE, FALSE))
+    return(sum(test_2$match))
   }
-  
-  my_df <- files_list3[["june-2021"]] %>%
-    janitor::row_to_names(1) %>%
-    clean_names()
-    # last is file_date, but has removed this because it did have a colname
-    names(my_df)[length(my_df)] <- "file_date"
  
+  # # change blank names- not working
+  # change_blank_names <- function(filename, i){
+  #   filename[[i]] <- filename[[i]] %>%
+  #     janitor::row_to_names(1) %>%
+  #     clean_names()
+  #   # last is file_date, but has removed this because it did have a colname
+  #   names(filename[[i]])[length(filename[[i]])] <- "file_date"
+  # }
+  
+  files_list3 <- files_list2
+  
+for(i in 1:length(files_list3)){
+  filename <- files_list3 # copying over the object not the name
+  
+  if(check_toprow_blank(filename, i) > 0) {
+    files_list3[[i]] <- files_list3[[i]] %>%
+      janitor::row_to_names(1) %>%
+      clean_names()
+    # last is file_date, but has removed this because it did have a colname
+    names(files_list3[[i]])[length(files_list3[[i]])] <- "file_date"
+  }
+}
 
   
